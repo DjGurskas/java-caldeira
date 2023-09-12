183bddd1c17217bd5700f39cb6ab202f5cea9839
DjGurskas <gurskasdjenifer@gmail.com>
2023-09-12 16:55:19 -0300

    att

---
+++ Javaa/javinha/javinhacaldeira/src/aula2/Calculator.java
@@ -0,0 +1,33 @@
+package src.aula2;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Coloque o primeiro número: ");
+        System.out.print("Coloque o segundo número: ");
+
+
+        System.out.println(".:Calculadora:.");
+        System.out.println("1 - Adição");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Divisão");
+        System.out.println("4 - Multiplicação");
+        System.out.println("5 - Raiz");
+        System.out.println("6 - Nenhuma das alternativas");
+        int opcao = scan.nextInt();
+
+        boolean calculator = false;
+
+        switch (opcao) {
+            case 1:
+
+        }
+
+
+    }
+
+}
