e5a3c3ec26dc0b877a1cfd8e3ab389c3b80df040
Djênifer Gurskas <gurskasdjenifer@gmail.com>
2023-09-21 00:52:32 -0300

    update
    tarefas adicionais ainda não feitas

--- Javaa/javinha/javinhacaldeira/src/aula3/ContaBancaria.java
+++ Javaa/javinha/javinhacaldeira/src/aula3/ContaBancaria.java
@@ -1,6 +1,6 @@
 package src.aula3;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 //Exercico 01 Aula 03
@@ -12,44 +12,64 @@ public class ContaBancaria {
     private String banco;
     private String endereco;
     private double saldo;
-    private int horaAtual;
+    private Date horaAtual;
 
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String datetime = DateTimeFormatter.ofPattern("MM DD YYYY, HH:MM:SS A")
-                .format(LocalDateTime.now());
-
 
     }
+
 
     public void Conta(String nome, int idConta, String cpf, String banco,
-    String endereco, double saldo, int horaAtual){
-        this.nome=nome;
-        this.idConta=idConta;
-        this.cpf=cpf;
-        this.banco=banco;
-        this.endereco=endereco;
-        this.saldo=saldo;
-        this.horaAtual=horaAtual;
+                      String endereco, double saldoInicial, Date horaAtual) {
+        this.nome = nome;
+        if (validarCPF(cpf)) {
+            this.cpf = cpf;
+        } else {
+            throw new IllegalArgumentException("CPF inválido!!");
+            //lança um novo argumento inválido (usado em casos onde recebe um input inválido)
+            //pode ser usado até em e-mail
+        }
+        this.idConta = idConta;
+        this.banco = banco;
+        this.endereco = endereco;
+        this.saldo = saldoInicial;
+        this.horaAtual = new Date();
 
     }
+
     public void saque(double valor) {
         if (this.saldo >= valor) {
             this.saldo -= valor;
         } else {
             System.out.println("Error: Saldo insuficiente!");
-        } System.out.println("Saldo atual: " + saldo);
+        }
+        System.out.println("Saldo atual: " + saldo);
     }
-    public void deposito(double valor){
+
+    public void deposito(double valor) {
         this.saldo = saldo + valor;
     }
 
-    public void pix(double valor){
-
+    public void pix(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+            System.out.println("Transferência pix realizada com sucesso.");
+        } else {
+            System.out.println("Saldo insuficiente para pix.");
+        }
     }
-    public boolean transferencia(ContaBancaria destino,double valor){
-        if(this.saldo >= valor) {
+
+    public void verificarHorario() {
+        SimpleDateFormat hours = new SimpleDateFormat("HH:mm:ss");
+        System.out.println("Horário atual: " + hours.format(horaAtual));
+        // SDF é uma definição que espera uma data que contenha dia/mês/ano
+        // ou horário que contenha hora/minutos/segundos
+    }
+
+    public boolean transferencia(ContaBancaria destino, double valor) {
+        if (saldo >= valor) {
             this.saldo -= valor;
             destino.deposito(valor);
             return true;
@@ -58,13 +78,32 @@ public class ContaBancaria {
         }
     }
 
-    public void verificarSaldo(){
-    System.out.println("Saldo atual é: " + saldo);
+    public void verificarSaldo() {
+        System.out.println("Saldo atual é: " + saldo);
     }
 
-    public void setHoraAtual(){
+    public void verificarInformacoes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Banco: " + banco);
+        System.out.println("ID Conta: " + idConta);
+        System.out.println("Endereço: " + endereco);
     }
-    public void verificarInformacoes(){
 
+    private boolean validarCPF(String CPF){
+        if (cpf.length() != 11){
+            return false;
+        }
+
+        for (char validar : cpf.toCharArray()) { //tá verificando se todos os caracteres são números
+            if (!Character.isDigit(validar)) {
+                return false;
+            }
+        }
+      return true;
     }
+
 }
+
+//fazer as tarefas adicionais
+
--- Javaa/javinha/javinhacaldeira/src/aula4/Veiculo.java
+++ Javaa/javinha/javinhacaldeira/src/aula4/Veiculo.java
@@ -1,6 +1,5 @@
 package src.aula4;
 
-import java.util.Scanner;
 
 public class Veiculo {
 
@@ -9,5 +8,42 @@ public class Veiculo {
     int ano;
 
 
+    public Veiculo(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+
+        Carro carro = new Carro("BMW", "430i", 2017);
+        Moto moto = new Moto("Honda", "CBR500R", 2021);
+        int distancia = 100;
+
+
+    }
+
+    public  abstract double calcularCustoViagem(int distancia);
 }
+
+    public class Carro extends Veiculo{
+    public Carro(String marca, String modelo, int ano) {
+        super(marca, modelo, ano);
+    }
+
+    @Override
+    public double calcularCustoViagem(int distancia) {
+        return distancia * 0.20;
+    }
+}
+
+    public class Moto extends Veiculo{
+        public Moto(String marca, String modelo, int ano){
+            super(marca, modelo, ano);
+        }
+
+        @Override
+        public double calcularCustoViagem(int distancia) {
+            return distancia * 0.15;
+        }
+    }
+//@Override” é uma “annotation” que pode ser acrescentada a um método que foi sobreposto.
+//é necessário para estender ou modificar a implementação abstrata. fonts: google
 
